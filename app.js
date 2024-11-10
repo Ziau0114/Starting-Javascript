1ce794a0b28cf75c6ed9a602019c653f3d3694a1
Ziau0114 <ziamaryam2526@gmail.com>
2024-11-10 16:51:23 +0500

    start my JS journey

---
+++ app.js
@@ -0,0 +1,18 @@
+const product = {
+    name: "black pan",
+    price: 19,
+    rating: 4,
+    offer: 5
+};
+let student = {
+    name: "Zia Uddin",
+    age: 16,
+    marks: 700/100,
+    isPass: true
+}
+let profile ={
+    userName: "Zia Uddin",
+    followors: 1562,
+    isFollow: true
+}
+console.log(profile);
