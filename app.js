149463f4eb1e25e2a78421d212c6de665a91260f
Ziau0114 <ziamaryam2526@gmail.com>
2024-11-14 12:38:55 +0500

    14-11-2024 Today I learn about Operators Conditional Statement

--- app.js
+++ app.js
@@ -16,3 +16,57 @@ let profile ={
     isFollow: true
 }
 console.log(profile);
+//post and pre concept
+// let a = 5
+// let b = 2
+
+// console.log( "a = 5", a++)
+// console.log(a)
+// console.log(++a)
+
+//assignment operators
+// let a = 5
+// a+= 5
+// a-= 5
+// a*= 2
+// a/= 5
+// a%= 5
+// a**= 2
+
+
+// console.log(a)
+
+//Conditional Statement
+
+// alert("Enter your age")
+
+//  let age = 12
+
+// if (age > 18) {
+//     document.write("you can vote")    
+// } else {
+//     document.write("You cannot vote")
+// }
+
+// let num = prompt("Enter the number")
+
+// if (num%5 === 0) {
+//     document.write(num, " is multiple to 5")
+    
+// } else {
+//     document.write(num, " is not multiple to 5")
+// }
+// let score = prompt("enter your marks")
+
+// if (score >= 80 && score <= 100) {
+//     document.write("You grade is A")
+    
+// } else if (score >= 70 && score <= 79) {
+//     document.write("You grade is B")
+// } else if (score >= 60 && score <= 69) {
+//     document.write("You grade is C")
+// }else if (score >= 60 && score <= 69) {
+//     document.write("You grade is D")
+// }else{
+//     document.write("Try Again")
+// }
