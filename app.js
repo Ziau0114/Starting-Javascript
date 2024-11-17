377e58667216ac4b67e20bac48f2bb6117b11624
Ziau0114 <ziamaryam2526@gmail.com>
2024-11-17 18:03:48 +0500

    Loops are completed with practice quastion

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const product = {
-    name: "black pan",
-    price: 19,
-    rating: 4,
-    offer: 5
-};
-let student = {
-    name: "Zia Uddin",
-    age: 16,
-    marks: 700/100,
-    isPass: true
-}
-let profile ={
-    userName: "Zia Uddin",
-    followors: 1562,
-    isFollow: true
-}
-console.log(profile);
+// const product = {
+//     name: "black pan",
+//     price: 19,
+//     rating: 4,
+//     offer: 5
+// };
+// let student = {
+//     name: "Zia Uddin",
+//     age: 16,
+//     marks: 700/100,
+//     isPass: true
+// }
+// let profile ={
+//     userName: "Zia Uddin",
+//     followors: 1562,
+//     isFollow: true
+// }
+// console.log(profile);
 //post and pre concept
 // let a = 5
 // let b = 2
@@ -69,4 +69,78 @@ console.log(profile);
 //     document.write("You grade is D")
 // }else{
 //     document.write("Try Again")
+
+
+
+
+// Loops in JS
+
+    // for (let i = 0;  i <= 5; i++) {
+    //   console.log("i =" ,i);
+        
+    // }
+
+// for (let i = 1; i <=100; i++) {
+//     console.log("i =", i);
+    
 // }
+
+// let sum = 0;
+// let i = 1;
+
+// for (let i = 0; i <=10; i++) {
+//    sum = sum + i
+    
+// }
+// console.log("sum =" , sum)
+
+// let sum = 0;
+// for (let i = 1;  i<=120; i++) {
+//      sum = sum + i //sum = 0 +1
+    
+// }
+// console.log("sum =", sum)
+
+//while loop 
+// let i = 0;
+// while (i <= 10) {
+//    console.log("Zia Uddin");
+//     i++;   
+// }
+
+
+
+
+//for--of loop
+// let str ="ZiaUddin"
+// let size = 0;
+
+// for (let i of str) {
+//     console.log("i =", i);
+//     size++
+    
+// }
+// console.log("size =", size);
+
+
+//practice loops
+
+// for (let i = 0;  i<=100; i++) {
+//    if (i % 2 !== 0) {
+//       console.log("i =", i);
+          
+//    } 
+   
+    
+// }
+
+//Q2
+
+// let gameNum = "50"
+// let userNum = prompt("Gusse the number? ")
+
+// while (userNum !== gameNum) {
+//     prompt("You entered the wrong number,Try again")
+    
+// }
+// document.write("Congratulation! You won the game")
